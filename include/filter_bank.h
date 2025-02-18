f72d228625bb820e611ca4174447680368a66487
Advait Koparkar <advaitkoparkar@gmail.com>
2025-02-17 17:34:08 -0800

    define buffer & sample types with some utils

--- include/filter_bank.h
+++ include/filter_bank.h
@@ -1,22 +1,27 @@
 #ifndef IIR_FACTORY_H
 #define IIR_FACTORY_H
+#include <stdint.h>
 
-typedef float dtype_t;
-typedef double acc_dtype_t;
+typedef float sample_t;
 
 typedef enum status_e {
     FB_SUCCESS = 0,
     FB_FAILURE = 1
 } status_t;
 
-typedef struct sample_s {
-    dtype_t val;
-    struct sample_s *next;
-} sample_t;
+/*
+    ring buffer implementation 
 
+    once initialized, ring buffer looks like this:
+        buf[0] => buf[1] => buf[2] => buf[0]
+                   ^ptr
+    
+    buf: circular buffer containing 3 samples
+    ptr: entry point to circular buffer - points to loc of earliest sample in ring buffer
+*/
 typedef struct buffer3_s {
-    sample_t *ptr;
-    sample_t *buffer[3];    
+    uint8_t ptr;
+    sample_t buffer[3];    
 } buffer3_t;
 
 typedef struct biquad_s {
@@ -28,10 +33,10 @@ typedef struct biquad_s {
 
 
 // init biquad
-status_t init_biquad(buffer3_t *a, buffer3_t *b, buffer3_t *state, biquad_t *bq);
+status_t init_biquad(buffer3_t *a, buffer3_t *b, biquad_t *bq);
 
 // process sample
-status_t process_sample(dtype_t *x, biquad_t *bq, dtype_t *y);
+status_t process_sample(sample_t *x, biquad_t *bq, sample_t *y);
 
 // close biquad
 status_t close_biquad(biquad_t *bq);
--- src/filter_bank.c
+++ src/filter_bank.c
@@ -1,7 +1,6 @@
 #include "../include/filter_bank.h"
 #include "stdlib.h"
 
-
 /*
     private functions
 */
@@ -11,14 +10,8 @@ static void free_buffer(buffer3_t *buf)
         return;
     }
     
-    free(buf->ptr);
-    buf->ptr = NULL;
-
-    int ctr = 0;
-    for (ctr = 0; ctr < 3; ++ctr) {
-        free(&buf->buffer[ctr]);
-        buf->buffer[ctr] = NULL;
-    }
+    free(buf);
+    buf = NULL;
 }
 
 static status_t rotate_buffer3(buffer3_t *buf)
@@ -28,16 +21,26 @@ static status_t rotate_buffer3(buffer3_t *buf)
         status = FB_FAILURE;
         goto EXIT;
     }
-    buf->ptr = buf->ptr->next;
+    buf->ptr = (buf->ptr+1)%3;
 EXIT:
-    if (status != FB_SUCCESS) {
-        free(buf);
-        buf = NULL;
-    }
     return status;
 }
 
-static status_t buffer3_prod(buffer3_t *a, buffer3_t *b, acc_dtype_t *result)
+static status_t push_buffer(sample_t x, buffer3_t *buf)
+{
+    status_t status = FB_SUCCESS;
+    if (!buf || !x) {
+        status = FB_FAILURE;
+        goto EXIT;
+    }
+
+    buf->buffer[buf->ptr] = x;
+    status = rotate_buffer3(buf);
+EXIT:
+    return status;
+}
+
+static status_t buffer3_prod(buffer3_t *a, buffer3_t *b, sample_t *result)
 {
     status_t status = FB_SUCCESS;
     if (a == NULL || b == NULL || result == NULL) {
@@ -46,24 +49,52 @@ static status_t buffer3_prod(buffer3_t *a, buffer3_t *b, acc_dtype_t *result)
     }
     
     int ctr = 0;
-    sample_t *a_ptr = a->ptr;
-    sample_t *b_ptr = b->ptr;
+    uint8_t a_ptr = (a->ptr+2)%3;
+    uint8_t b_ptr = (b->ptr+2)%3;
+    *result = 0.0;
     for (ctr = 0; ctr < 3; ++ctr) {
-        if (a_ptr == NULL || b_ptr == NULL) {
-            status = FB_FAILURE;
-            goto EXIT;
-        }
+        sample_t a_val = a->buffer[a_ptr++];
+        sample_t b_val = b->buffer[b_ptr++];
+        *result += (a_val*b_val);
 
-        *result += a_ptr->val * b_ptr->val;        
+        a_ptr %= 3;
+        b_ptr %= 3;
     }
 
 EXIT:
     if (status != FB_SUCCESS) {
-        free(a);
-        free(b);
-        a = NULL;
-        b = NULL;
         result = NULL;
+    }
+    return status;
+}
+
+static void free_biquad(biquad_t *bq)
+{
+    if (!bq) return;
+    free_buffer(&bq->a);
+    free_buffer(&bq->b);
+    free_buffer(&bq->x);
+    free_buffer(&bq->y);
+    bq = NULL;
+}
+
+
+status_t init_biquad(buffer3_t *a, buffer3_t *b, biquad_t *bq)
+{
+    status_t status = FB_SUCCESS;
+    if (!a || !b) {
+        status = FB_FAILURE;
+        goto EXIT;
+    }
+
+    bq = malloc(sizeof(biquad_t));
+
+    // init buffers
+    bq->a = *a;
+    bq->b = *b;
+EXIT:
+    if (status != FB_SUCCESS) {
+        free_biquad(bq);
     }
     return status;
 }
